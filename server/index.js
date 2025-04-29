edbd49634a89ffd50ac4231936470399d4c89bcd
bostiog1 <teo.bostiog@yahoo.com>
2025-04-29 20:42:49 +0300

    1. Restructuring the files 2. Connecting the database to the backend

--- server/index.js
+++ server/index.js
@@ -9,4 +9,21 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+const CONNECTION_URL =
+  "mongodb+srv://teooboost:teooboost123@cluster0.sgd0sce.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
 // Routes
+
+// z1ftkBJwc9u664Uk
