59dcb507bba737581c5ba121952a2959b96bd84a
bostiog1 <teo.bostiog@yahoo.com>
2025-05-01 01:38:07 +0300

    Installed Axios dependency and created structure for the frontend part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,52 @@
-import { Container } from "@mui/material-ui/core";
+import {
+  AppBar,
+  Container,
+  Grid,
+  Grow,
+  makeStyles,
+  Typography,
+} from "@mui/material";
+import memories from "../public/assets/memories.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
+
 const App = () => {
-  return <Container>
-    
-  </Container>;
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid xs={12} sm={7}>
+              <Posts />
+            </Grid>
+
+            <Grid xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 };
 
 export default App;
