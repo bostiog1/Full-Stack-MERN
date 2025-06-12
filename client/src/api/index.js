f395eee2938533a90df505cc1dd401570ff7ef0d
bostiog1 <teo.bostiog@yahoo.com>
2025-06-12 23:04:15 +0300

    frontend api update

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const API = axios.create({ baseURL: "http://localhost:5000" });
 const API = axios.create({
-  baseURL: "https://full-stack-mern-7no2.vercel.app",
+  baseURL: "https://full-stack-mern-y75x.vercel.app",
   // withCredentials: true,
 });
 
